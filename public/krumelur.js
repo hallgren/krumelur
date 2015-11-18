0c560d6b0fb3703095654e243203aa94bc17fbbf
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-11-18 13:26:54 +0100

    removed snapshot method

--- public/krumelur.js
+++ public/krumelur.js
@@ -13725,20 +13725,13 @@ var convertHTML = require('html-to-vdom')({
 });
 
 var Krumelur = {
-  version: "0.0.1",
-  vTrees: {}
-};
-
-Krumelur.snapshot = function(element) {
-  if (!element.id) { throw "Krumelur.initialize: element most have id defined"; }
-  Krumelur.vTrees[element.id] = convertHTML(element.outerHTML.trim());
+  version: "0.0.2"
 };
 
 Krumelur.apply = function(htmlString, element) {
   var replacementVtree = convertHTML(htmlString.trim());
-  var patches = diff(Krumelur.vTrees[element.id], replacementVtree);
+  var patches = diff(convertHTML(element.outerHTML.trim()), replacementVtree);
   element = patch(element, patches);
-  Krumelur.vTrees[element.id] = replacementVtree;
 };
 
 module.exports = global.Krumelur = Krumelur
