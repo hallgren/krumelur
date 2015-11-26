d730a197908230c9ea5a7788ba0b9d3584d839be
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-11-26 14:27:09 +0100

    added snapshot and applyFromSnapshot and some cats

--- public/krumelur.js
+++ public/krumelur.js
@@ -13725,10 +13725,25 @@ var convertHTML = require('html-to-vdom')({
 });
 
 var Krumelur = {
-  version: "0.0.3"
+  version: "0.1.0",
+  vTrees: {}
+};
+
+Krumelur.snapshot = function(element) {
+  if (!element.id) { throw "Krumelur.snapshot: element most have id defined"; }
+  var vTree = convertHTML(element.outerHTML.trim());
+  Krumelur.vTrees[element.id] = vTree;
+};
+
+Krumelur.applyFromSnapshot = function(htmlString, element) {
+  if (!element.id) { throw "Krumelur.applyFromSnapshot: element most have id defined"; }
+  var replacementVtree = convertHTML(htmlString.trim());
+  var patches = diff(Krumelur.vTrees[element.id], replacementVtree);
+  element = patch(element, patches);
 };
 
 Krumelur.apply = function(htmlString, element) {
+  if (!element.id) { throw "Krumelur.apply: element most have id defined"; }
   var replacementVtree = convertHTML(htmlString.trim());
   var patches = diff(convertHTML(element.outerHTML.trim()), replacementVtree);
   element = patch(element, patches);
