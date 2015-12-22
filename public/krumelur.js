5cb53fa4c833dc7a9270dafaffc4fcfeddeef8a6
Morgan Hallgren <morgan.hallgren@gmail.com>
2015-12-22 13:42:00 +0100

    html-to-vdom -> vdom-parser

--- public/krumelur.js
+++ public/krumelur.js
@@ -14420,20 +14420,29 @@ var Krumelur = {
   vTrees: {}
 };
 
-Krumelur.snapshot = function(element) {
-  if (!element.id) { throw "Krumelur.snapshot: element most have id defined"; }
-  Krumelur.vTrees[element.id] = parser(element);
+Krumelur.snapshot = function(element, vTree) {
+  if (!element.id) { throw "Krumelur.snapshot: element must have id defined"; }
+  Krumelur.vTrees[element.id] = vTree || parser(element);
 };
 
-Krumelur.applyFromSnapshot = function(htmlString, element) {
-  if (!element.id) { throw "Krumelur.applyFromSnapshot: element most have id defined"; }
+Krumelur.applyFromSnapshot = function(htmlString, element, snapshotCallback) {
+  if (!element.id) { throw "Krumelur.applyFromSnapshot: element must have id defined"; }
   var replacementVtree = parser(htmlString) ;
   var patches = diff(Krumelur.vTrees[element.id], replacementVtree);
   patch(element, patches);
+
+  if (snapshotCallback) {
+    setTimeout(function(){
+      Krumelur.snapshot(element, replacementVtree);
+      if (typeof snapshotCallback == "function") {
+        snapshotCallback();
+      }
+    }, 50);
+  }
 };
 
 Krumelur.apply = function(htmlString, element) {
-  if (!element.id) { throw "Krumelur.apply: element most have id defined"; }
+  if (!element.id) { throw "Krumelur.apply: element must have id defined"; }
   var replacementVtree = parser(htmlString);
   var patches = diff(parser(element), replacementVtree);
   patch(element, patches);
